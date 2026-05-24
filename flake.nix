5e5f82fe9b3c08def8de28c9af5d162c36521630
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2026-05-24 21:59:13 +0900

    introduce flake-parts

--- flake.nix
+++ flake.nix
@@ -1,62 +1,66 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     uno-ls.url = "github:hooreique/unocss-language-server";
   };
 
   outputs =
     inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = inputs.nixpkgs.legacyPackages.${system};
-        flakeRef = "github:hooreique/commut";
-        installFlakeRef = "${flakeRef}#commut-installer";
-        fonts = pkgs.callPackage ./hack-woff2.nix { };
-        prepare-fonts = pkgs.callPackage ./prepare-fonts.nix {
-          inherit fonts;
-        };
-        client = pkgs.callPackage ./frontend/package.nix {
-          inherit fonts;
-        };
-        commut = pkgs.callPackage ./backend/package.nix {
-          clientPackage = client;
-        };
-        installer = pkgs.callPackage ./installer/package.nix {
-          backendPackage = commut;
-          clientPackage = client;
-          installFlakeRef = installFlakeRef;
-        };
-      in
-      {
-        packages = {
-          default = commut;
-          commut = commut;
-          commut-installer = installer;
-        };
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
 
-        apps.prepare-fonts = inputs.flake-utils.lib.mkApp {
-          drv = prepare-fonts;
-        };
+      perSystem =
+        { pkgs, system, ... }:
+        let
+          flakeRef = "github:hooreique/commut";
+          installFlakeRef = "${flakeRef}#commut-installer";
+          fonts = pkgs.callPackage ./hack-woff2.nix { };
+          prepare-fonts = pkgs.callPackage ./prepare-fonts.nix {
+            inherit fonts;
+          };
+          client = pkgs.callPackage ./frontend/package.nix {
+            inherit fonts;
+          };
+          commut = pkgs.callPackage ./backend/package.nix {
+            clientPackage = client;
+          };
+          installer = pkgs.callPackage ./installer/package.nix {
+            backendPackage = commut;
+            clientPackage = client;
+            installFlakeRef = installFlakeRef;
+          };
+        in
+        {
+          packages = {
+            default = commut;
+            commut = commut;
+            commut-installer = installer;
+          };
 
-        devShells.default = pkgs.mkShell {
-          packages = [
-            pkgs.nodejs_24
-            pkgs.pnpm
-            pkgs.typescript
-            pkgs.typescript-language-server
-            pkgs.cargo
-            pkgs.clippy
-            pkgs.pkg-config
-            pkgs.rust-analyzer
-            pkgs.rustc
-            pkgs.rustfmt
-            pkgs.caddy
-            pkgs.woff2
-            inputs.uno-ls.packages.${system}.default
-          ];
+          apps.prepare-fonts.program = prepare-fonts;
+
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.nodejs_24
+              pkgs.pnpm
+              pkgs.typescript
+              pkgs.typescript-language-server
+              pkgs.cargo
+              pkgs.clippy
+              pkgs.pkg-config
+              pkgs.rust-analyzer
+              pkgs.rustc
+              pkgs.rustfmt
+              pkgs.caddy
+              pkgs.woff2
+              inputs.uno-ls.packages.${system}.default
+            ];
+          };
         };
-      }
-    );
+    };
 }
